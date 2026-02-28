4bec9aa653508f6f1a3cd90bbe4bcee31decb2a0
Akhyar <ayozzet@gmail.com>
2026-02-28 12:44:12 +0800

    Implement RGB control based on potentiometer input
    Added RGB output control based on potentiometer value.

--- 07 - Potentiometer with RGB Output/pot_RGB_output/pot_RGB_output.ino
+++ 07 - Potentiometer with RGB Output/pot_RGB_output/pot_RGB_output.ino
@@ -1,12 +1,36 @@
 int pot = 4;
+int r = 5;
+int g = 6;
+int b = 7;
 
 void setup() {
   pinMode(pot, INPUT);
-  Serial.begin(115200);
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
 }
 
 void loop() {
-  int pot = analogRead(pot);
-  Serial.println(pot);
-  delay(100);
+  int potVal = analogRead(pot);
+  if (potVal <1000){
+    digitalWrite(r, LOW);
+    digitalWrite(g, LOW);
+    digitalWrite(b, LOW);
+  }
+  if ((potVal >= 1000) && (potVal < 2000)){
+    digitalWrite(r, HIGH);
+    digitalWrite(g, LOW);
+    digitalWrite(b, LOW);
+  }
+  if ((potVal >= 2000) && (potVal < 3000)){
+    digitalWrite(r, LOW);
+    digitalWrite(g, HIGH);
+    digitalWrite(b, LOW);
+  }
+  else{
+    digitalWrite(r, LOW);
+    digitalWrite(g, LOW);
+    digitalWrite(b, HIGH);
+  }
+  
 }
