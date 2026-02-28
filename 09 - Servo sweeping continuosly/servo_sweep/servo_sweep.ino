4961481682b09c95412ff5b4310a957e8ccc8adf
Akhyar <ayozzet@gmail.com>
2026-02-28 12:48:17 +0800

    Implement continuous servo sweeping in loop

--- 09 - Servo sweeping continuosly/servo_sweep/servo_sweep.ino
+++ 09 - Servo sweeping continuosly/servo_sweep/servo_sweep.ino
@@ -16,12 +16,12 @@ void setup() {
 }
 
 void loop() {
-	int state = digitalRead(btn);
-	if (state == 1) {
-		myservo.write(0);
-		delay(500);
-	} else {
-		myservo.write(180);
-		delay(500);
+	for (pos = 0; pos <= 180; pos += 1) { 
+		myservo.write(pos);    
+		delay(15);             
+	}
+	for (pos = 180; pos >= 0; pos -= 1) { 
+		myservo.write(pos);    
+		delay(15);             
 	}
 }
